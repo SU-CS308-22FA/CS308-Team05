c4e2fc00572a4b08a0ae6140e65554a3ed82e5bb
egemencakir-SU <egemencakir@sabanciuniv.edu>
2022-11-02 15:35:37 +0300

    navigate system is added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React, { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
-import { Login } from "./Login";
-import { Sign_Up } from "./Sign_Up";
+import { Login } from "./pages/Login";
+import { Sign_Up } from "./pages/Sign_Up";
+import { User } from "./pages/User";
+import {BrowserRouter as Router, Switch, Route, Link, NavLink} from "react-router-dom";
 
 function App() {
-  const [currentForm, setCurrentForm] = useState('login');
-
-  const toggleForm = (formName) => {
-    setCurrentForm(formName);
-  }
 
   return (
-    <div className = "App">
+    <Router>
+      <div className="App">
       <img src="/images/Logo.png" alt=""/>
-      {
-        currentForm === "login" ? <Login onFormSwitch={toggleForm}/> : <Sign_Up onFormSwitch={toggleForm}/>
-      }
-      <img src="/images/rate12.png" alt=""/>
-    </div>
+        <ul>
+          <li>
+    
+          </li>
+          <li>
+         
+          </li>
+        </ul>
+
+        <Switch>
+          <Route exact path="/login">
+              <Login />
+          </Route>
+          <Route path="/signup">
+            <Sign_Up />
+          </Route>
+        </Switch>
+        <img src="/images/rate12.png" alt=""/>
+      </div>
+    </Router>
   );
 }
 
