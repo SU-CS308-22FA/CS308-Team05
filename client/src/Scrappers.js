e22ca74d74f32f8086d7432779f8800f5ab295bf
egemencakir-SU <egemencakir@sabanciuniv.edu>
2022-11-20 23:32:29 +0300

    scrappers output is tansformed into a txt

--- client/src/Scrappers.js
+++ client/src/Scrappers.js
@@ -1,33 +1,35 @@
 const puppeteer = require('puppeteer');
-/******************************************************************************************************************/
-var GET_11 = async function (page,st11,st12,st3){
+const fs = require('fs');
+//import puppeteer
+
+
+/*****************************************************************************************************************/
+var GET_11 = async function (page, st11, st12, st3) {
     var isimler_SOL = []
     var isimler_SAG = []
     for (var i = 1; i < 13; i++) {
         try {
             var st2 = i;
             var [el] = await page.$x(st11 + st2 + st3);
-            if (el != null)
-            {
+            if (el != null) {
                 var txt = await el.getProperty('textContent');
                 var rawTxt = await txt.jsonValue();
                 isimler_SOL.push(rawTxt);
             }
-            if (el != null)
-            {
+            if (el != null) {
                 var [el] = await page.$x(st12 + st2 + st3);
                 var txt = await el.getProperty('textContent');
                 var rawTxt = await txt.jsonValue();
                 isimler_SAG.push(rawTxt);
             }
 
-        } catch (err) { console.error(err.message);}
+        } catch (err) { console.error(err.message); }
     }
-    var LISTE = []; 
+    var LISTE = [];
     LISTE.push(isimler_SOL);
     LISTE.push(isimler_SAG);
     return LISTE;
-}
+};
 /******************************************************************************************************************/
 var GET_YEDEK = async function (page, st11, st12, st3,st4) {
     var isimler_SOL = []
@@ -83,12 +85,11 @@ var GET_TRAINER = async function (page, st11, st12, st3) {
     return isimler;
 }
 /******************************************************************************************************************/
-var start = async function (url) {
-
+var start = async function (url,stream) {
     const browser = await puppeteer.launch();
     const page = await browser.newPage()
     await page.goto(url)
-    
+    /*******************************************************************************************/
     var st11 = new String('//*[@id="main"]/main/div[6]/div/div/div[1]/div[3]/div[2]/div[');
     var st12 = new String('//*[@id="main"]/main/div[6]/div/div/div[2]/div[3]/div[2]/div[');
     var st3 = new String(']/div[2]/span/a');
@@ -99,7 +100,13 @@ var start = async function (url) {
     for (var i = 0; i < XX[0].length; i++) {console.log("\t"+XX[0][i]);}
     console.log("\nOyuncular sag: ");
     for (var i = 0; i < XX[1].length; i++) { console.log("\t" + XX[1][i]); }
-    console.log("-------------------------------------------------------- ");
+    console.log("---------------------------------------");
+
+    stream.write("\nOyuncular sol: "+"\n");
+    for (var i = 0; i < XX[0].length; i++) { stream.write("\t" + XX[0][i] + "\n"); }
+    stream.write("\nOyuncular sag: " + "\n");
+    for (var i = 0; i < XX[1].length; i++) { stream.write("\t" + XX[1][i] + "\n"); }
+    stream.write("---------------------------------------" + "\n");
     /*******************************************************************************************/
     var st11 = "//*[@id='main']/main / div[6] / div / div / div[1] / div[4] / table / tbody / tr["
     var st12 = "//*[@id='main']/main / div[6] / div / div / div[2] / div[4] / table / tbody / tr["
@@ -112,7 +119,13 @@ var start = async function (url) {
     for (var i = 0; i < XX[0].length; i++) { console.log("\t" + XX[0][i]); }
     console.log("\nYEDEKLER sag: ");
     for (var i = 0; i < XX[1].length; i++) { console.log("\t" + XX[1][i]); }
-    console.log("-------------------------------------------------------- ");
+    console.log("---------------------------------------");
+
+    stream.write("\nYEDEKLER sol: " + "\n");
+    for (var i = 0; i < XX[0].length; i++) { stream.write("\t" + XX[0][i] + "\n"); }
+    stream.write("\nYEDEKLER sag: " + "\n");
+    for (var i = 0; i < XX[1].length; i++) { stream.write("\t" + XX[1][i] + "\n"); }
+    stream.write("---------------------------------------" + "\n");
     /*******************************************************************************************/
     var st11 = "//*[@id='main']/main/div[6]/div/div/div[1]/div[4]/table/"; 
     var st12 = "//*[@id='main']/main/div[6]/div/div/div[2]/div[4]/table/"; 
@@ -122,19 +135,21 @@ var start = async function (url) {
 
     console.log("\nTEKNIK DIREKTOR sol: \t" + XX[0]);
     console.log("\nTEKNIK DIREKTOR sag: \t" + XX[1]); 
-    console.log("-------------------------------------------------------- ");
+    console.log("---------------------------------------");
+
+    stream.write("\nTEKNIK DIREKTOR sol: \t" + XX[0] + "\n");
+    stream.write("\nTEKNIK DIREKTOR sag: \t" + XX[1] + "\n");
+    stream.write("---------------------------------------" + "\n");
     /*******************************************************************************************/
     browser.close();
 };
 /******************************************************************************************************************/
 (
     async () => {
-        await start("https://www.transfermarkt.com/istanbulspor_trabzonspor/index/spielbericht/3854849");
+        var stream = fs.createWriteStream("JAW.txt");
+        await start("https://www.transfermarkt.com/istanbulspor_trabzonspor/index/spielbericht/3854849",stream);
         console.log("_________________________________________________");
-        await start("https://www.transfermarkt.com/sivasspor_gaziantep-fk/index/spielbericht/3854846");
-
-
-        console.log("_________________________________________________");
-        await start("https://www.transfermarkt.com/spielbericht/index/spielbericht/3860336");
+        await start("https://www.transfermarkt.com/sivasspor_gaziantep-fk/index/spielbericht/3854846",stream);
+        stream.end();
     }
 )();
