e224d3274056cd0b68e847ac97ab7c797064d8d2
Eren Uluç <erenuluc@Erens-MacBook-Pro-4.local>
2022-12-11 20:40:13 +0300

    Started writing Rating System by displaying the active matches

--- server/index.js
+++ server/index.js
@@ -512,3 +512,50 @@ app.post("/deactivateuser", (req, res) => {
     }
   });
 });
+
+app.get ("/getactivematches", (req, res) => {
+  db.query(
+    "SELECT * FROM MatchScores WHERE RateAvailable IS TRUE",
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res.send({message: "An error occured"});
+      } else {
+        res.send(result);
+      }
+    });
+});
+
+app.post ("/displayplayers", (req, res) => {
+  const server = req.body.server;
+  let table = "SELECT PlayerName FROM "+ server +" WHERE PlayerName IS NOT NULL";
+
+  db.query(
+    table,
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res.send({message: "An error occured"});
+      } else {
+        res.send(result);
+      }
+    });
+});
+
+app.post ("/savevote", (req, res) => {
+  const server = req.body.server;
+  const name = req.body.name;
+  
+  let table = "ALTER TABLE "+ server +" ADD COLUMN "+name+" INT(11) NULL AFTER `Position`";
+
+  db.query(
+    table,
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res.send({message: "An error occured"});
+      } else {
+        res.send(result);
+      }
+    });
+});
