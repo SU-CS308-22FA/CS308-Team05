a53244bee6a8d5aa6453069240eafb54b6f5ac78
ErsanAlp <ersanalp@sabanciuniv.edu>
2022-12-14 14:06:14 +0300

    player positions also given

--- client/src/teams/Trabzonspor.js
+++ client/src/teams/Trabzonspor.js
@@ -13,58 +13,68 @@ export const Trabzonspor = () => {
 
         <img src = "https://img.a.transfermarkt.technology/portrait/header/69646-1663875743.png?lm=1" height = "70px" weight = "70px" alt=""/>
         <p>
-        Jens Stryger Larsen
+        Jens Stryger Larsen:
+        Right Back
         </p>
 
-        <p>
         <img src = "https://img.a.transfermarkt.technology/portrait/header/149752-1630527249.png?lm=1" height = "70px" weight = "70px" alt=""/>
-        Vitor Hugo
+        <p>
+        Vitor Hugo:
+        Centre Back
         </p>
 
         <p>
         <img src = "https://img.a.transfermarkt.technology/portrait/header/182688-1630526199.png?lm=1" height = "70px" weight = "70px" alt=""/>
-        Manolis Siopis
+        Manolis Siopis:
+        Defensive Midfield
         </p>
 
-        <p>
         <img src = "https://img.a.transfermarkt.technology/portrait/header/275935-1630526123.png?lm=1" height = "70px" weight = "70px" alt=""/>
-        Dorukhan Toköz
+        <p>
+        Dorukhan Toköz:
+        Central Midfield
         </p>
 
         <img src = "https://img.a.transfermarkt.technology/portrait/header/616894-1663875962.png?lm=1" height = "70px" weight = "70px" alt=""/>
         <p>
         Eren Elmali:
-        Left back
+        Left Back
         </p>
 
-        <p>
         <img src = "https://img.a.transfermarkt.technology/portrait/header/234189-1644332670.png?lm=1" height = "70px" weight = "70px" alt=""/>
-        Mahmoud Trezeguet
+        <p>
+        Mahmoud Trezeguet:
+        Left Wing
         </p>
 
         <p>
         <img src = "https://img.a.transfermarkt.technology/portrait/header/203412-1668183778.jpg?lm=1" height = "70px" weight = "70px" alt=""/>
-        Andreas Cornelius
+        Andreas Cornelius:
+        Centre Forward
         </p>
 
-        <p>
         <img src = "https://img.a.transfermarkt.technology/portrait/header/109217-1643036892.jpeg?lm=1" height = "70px" weight = "70px" alt=""/>
-        Edin Visca
+        <p>
+        Edin Visca:
+        Right Wing
         </p>
 
-        <p>
         <img src = "https://img.a.transfermarkt.technology/portrait/header/271495-1630525402.png?lm=1" height = "70px" weight = "70px" alt=""/>
-        Abdülkadir Ömür
+        <p>
+        Abdülkadir Ömür:
+        Attack Midfield
         </p>
 
-        <p>
         <img src = "https://img.a.transfermarkt.technology/portrait/header/162571-1630525354.png?lm=1" height = "70px" weight = "70px" alt=""/>
-        Anastasios Bakasetas
+        <p>
+        Anastasios Bakasetas:
+        Attack Midfield
         </p>
 
-        <p>
         <img src = "https://img.a.transfermarkt.technology/portrait/medium/5078-1663860235.jpeg?lm=1" height = "70px" weight = "70px" alt=""/>
-        Abdullah Avcı
+        <p>
+        Abdullah Avcı:
+        Technic Director
         </p>
 
         <button onClick={() => history.push('/')}>Sign Out</button>
