53c0f33594e6763246ecb046daff253bf98c42a9
egemencakir-SU <egemencakir@sabanciuniv.edu>
2023-01-02 00:16:50 +0300

    team logos are added

--- server/index.js
+++ server/index.js
@@ -636,7 +636,7 @@ app.get('/fixtures_w6', (req, res) => {
   })
 })
 app.get('/PLAYERPAGE', (req, res) => {
-  db.query("SELECT * FROM PLAYERPAGE WHERE zmatch = 1", (err, result) => {
+  db.query("SELECT * FROM AlanyasporKayserispor", (err, result) => {
     if (err) {
       console.log(err)
     } else{
@@ -646,7 +646,7 @@ app.get('/PLAYERPAGE', (req, res) => {
 })
 
 app.get('/PLAYERPAGE_v2', (req, res) => {
-  db.query("SELECT * FROM PLAYERPAGE WHERE zmatch = 2", (err, result) => {
+  db.query("SELECT * FROM FatihKaragümrükTrabzonspor", (err, result) => {
     if (err) {
       console.log(err)
     } else{
