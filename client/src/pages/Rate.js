e7576e2e8554306d229748232acc8ee7867bb540
Eren Uluç <erenuluc@Erens-MacBook-Pro-4.local>
2023-01-04 01:51:27 +0300

    Solved match selection bug in rate.js

--- client/src/pages/Rate.js
+++ client/src/pages/Rate.js
@@ -8,6 +8,8 @@ const { Panel } = Collapse;
 export const Rate = (props) => {
     const [match, setMatch] = useState("");
     const [matches, setMatches] = useState("");
+    const [everymatch, setEverymatch] = useState([]);
+    const [message, setMessage] = useState("");
 
     Axios.defaults.withCredentials = true;
 
@@ -24,6 +26,7 @@ export const Rate = (props) => {
                 console.log(response);
                 let possiblematches = "/";
                 for (var i=0; i<response.data.length; i++){
+                    everymatch.push(response.data[i].HomeTeam + " " + response.data[i].HomeGoal + "-" + response.data[i].AwayGoal + " " + response.data[i].AwayTeam);
                     possiblematches = possiblematches + response.data[i].HomeTeam + " " + response.data[i].HomeGoal + "-" + response.data[i].AwayGoal + " " + response.data[i].AwayTeam + "/";
                 }
                 setMatches(possiblematches);
@@ -32,9 +35,18 @@ export const Rate = (props) => {
     };
 
     const selectmatch = () => {
-        global.match = match;
-        console.log(global.match);
-        history.push('/PlayerRate');
+        let inputcheck = false;
+        for (var i=0; i<everymatch.length; i++){
+            if (everymatch[i] === match){
+                inputcheck = true;
+            }
+        }
+        if (inputcheck){
+            global.match = match;
+            console.log(global.match);
+            history.push('/PlayerRate');
+        }
+        setMessage("Wrong input! Please try again!");
     };
 
     return (
@@ -47,6 +59,7 @@ export const Rate = (props) => {
                 <div></div>
                 <input onChange={(e) => setMatch(e.target.value)} name = "name" id = "name" placeholder = "A 3-3 B"/>
                 <p><button onClick={selectmatch}>Choose this match</button></p>
+                <p> {message} </p>
             </form>
             <button className = "link-btn" onClick={() => history.push('/User')}> Go back to user page </button>
         </div>
