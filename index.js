24ba1a78b7a14939a000e14b9d3ac14874db5b9e
Eren Uluç <erenuluc@Erens-MacBook-Pro-4.local>
2023-01-05 12:30:08 +0300

    a

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: ["http://localhost:3000"],
+  origin: ["https://cs308-renderclient.onrender.com"],
   methods: ["GET", "POST"],
   credentials: true,
 }));
